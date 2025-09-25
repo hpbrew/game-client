c5b6d9901343481fedfa546a874449f734c7c644
Hampton <hpbrew@gmail.com>
2025-09-25 16:21:56 -0500

    publish

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,35 @@
-import { defineConfig } from 'vite';
-import { createHtmlPlugin } from 'vite-plugin-html';
+import { defineConfig } from "vite"
+import { createHtmlPlugin } from "vite-plugin-html"
 
 export default defineConfig({
-  plugins: [createHtmlPlugin({
-    inject: {
-      injectOptions: {
-        tags: [
-          {
-            tag: 'script',
-            attrs: {
-              src: '/src/main.js',
+  base: "./game-client/",
+  plugins: [
+    createHtmlPlugin({
+      inject: {
+        injectOptions: {
+          tags: [
+            {
+              tag: "script",
+              attrs: {
+                src: "/src/main.js",
+              },
             },
-          },
-          {
-            tag: 'link',
-            attrs: {
-              rel: 'stylesheet',
-              href: '/styles.css',
+            {
+              tag: "link",
+              attrs: {
+                rel: "stylesheet",
+                href: "/styles.css",
+              },
             },
-          },
-        ],
+          ],
+        },
       },
-    },
-  })],
+    }),
+  ],
   server: {
     open: true,
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
-});
+})
