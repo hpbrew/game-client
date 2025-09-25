991b80e27ed82c0620271867b649be58b4a7fde2
Hampton <hpbrew@gmail.com>
2025-09-25 17:11:49 -0500

    adjusting url again

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite"
 import { createHtmlPlugin } from "vite-plugin-html"
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/game-client" : "/",
+  base: process.env.NODE_ENV === "production" ? "/game-client/" : "/",
   plugins: [
     createHtmlPlugin({
       inject: {
